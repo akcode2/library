bd57ddd994917fcaa186b7869ac60d575b6d5276
akcode2 <40044933+akcode2@users.noreply.github.com>
2023-06-13 12:06:15 -0500

    Complete styling

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
 
-
+// Get the form
 const form = document.querySelector('form');
-
+// When form is submitted, add Book and display it
 form.addEventListener('submit', event => {
     event.preventDefault();
 
@@ -41,49 +41,55 @@ form.addEventListener('submit', event => {
     updateLibrary();
 })
 
+// Remove book from myLibrary 
 function removeBookFromLibrary(index) {
     // Remove 1 element at 'index' position
     myLibrary.splice(index, 1);
 }
 
+// Update 'read' from true to false and vice versa
 function changeReadState(index) {
     const state = myLibrary[index].read;
     if (state === true) {
         myLibrary[index].read = false;
-        console.log(`${myLibrary[index].read}`);
     }
     else {
         myLibrary[index].read = true;
-        console.log(`${myLibrary[index].read}`);
     }
-
     return myLibrary[index].read;
 }
 
 
 function updateLibrary() {
+    // Get the library
     const library = document.getElementById('library');
+    // Get the index of the latest Book object
     const latestBook = myLibrary.length - 1;
 
+    // Create card and add text
     const card = document.createElement('article');
     card.classList.add('card');
     const cardTitle = document.createElement('p');
     cardTitle.innerText = myLibrary[latestBook].info();
 
+    // Create a div to contain buttons
     const cardBtns = document.createElement('div');
     cardBtns.classList.add('cardBtns');
 
+    // Create a 'remove' button
     const removeBtn = document.createElement('button');
     removeBtn.classList.add('removeBtn');
+    // Use an image to style the button
     const removeSVG = document.createElement('img');
     removeSVG.src = "./images/remove.svg";
     removeBtn.appendChild(removeSVG);
-
+    // When 'remove' is clicked, delete the Book object and remove it from DOM
     removeBtn.addEventListener('click', () => {
         removeBookFromLibrary(card.id);
         card.remove();
     })
 
+    // Create a 'read' state button that toggles the 'read' state
     const readBtn = document.createElement('button');
     readBtn.classList.add('readBtn');
     const readSVG = document.createElement('img');
@@ -107,10 +113,13 @@ function updateLibrary() {
         }
     })
 
-    cardBtns.appendChild(readBtn);
+    // cardTitle.insertAdjacentElement('beforebegin', readBtn);
+    
+    // Add the remove button to the buttons div
     cardBtns.appendChild(removeBtn);
 
     card.appendChild(cardBtns);
+    card.appendChild(readBtn)
     card.appendChild(cardTitle);
     card.id = latestBook;
     library.appendChild(card);
@@ -123,6 +132,32 @@ myLibrary.push(book1);
 
 updateLibrary();
 
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("add");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
 
 
 
